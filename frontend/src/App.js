6045698c646127102af0c4bf6373186a0acecadc
jinsoo <jinsoo1004@proton.me>
2023-05-10 17:45:00 +0900

    뮁

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+const DoA = () => {
+  return (
+    <div>
+      <h1>DoA</h1>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <DoA />
+    </div>
+  );
+}
+
+export default App;
