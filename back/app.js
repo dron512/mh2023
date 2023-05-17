13415f80094a6aec91266c8f2c3ed0e27734ab76
jcho0127 <josangyoung267@gmail.com>
2023-05-17 17:49:15 +0900

    📌 :: middle save
    2023/05/17

--- back/app.js
+++ back/app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const cors = require('cors');
 
 const indexRouter = require('./router/inedexRouter');
+const boardRouter = require('./router/boardRouter')
+
+require('./db/database');
+
 
 const app = express();
 const port = 3001;
 
 // node app.js
 app.use(cors());
+
 app.use("/",indexRouter);
+app.use("/board",boardRouter);
 
 app.listen(port, () => {
     console.log("server running on 3001");
